a72315e3d3bbe90276750714f34ace14316a0dff
Ac-11 <chatterjiabhisth@gmail.com>
2024-12-14 10:06:48 +0530

    changers

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Enable CORS to allow requests from the front-end
-app.use(cors());
+app.use(cors({
+  origin: 'https://d301jqwfg3cytj.cloudfront.net/', // Replace with your CloudFront domain
+  methods: ['GET', 'POST'], // Specify allowed HTTP methods
+  credentials: true // Allow cookies if needed
+}));
 
 // Define a simple API endpoint
 app.get('/', (req, res) => {
