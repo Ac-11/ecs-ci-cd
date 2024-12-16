519f2666319fa8a8c6988570ea5a7da92cb9f7c5
Ac-11 <chatterjiabhisth@gmail.com>
2024-12-16 07:50:47 +0530

    chnages

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 // Define a simple API endpoint
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from the ECS Fargate backend-backend!' });
+  res.json({ message: 'Hello from the ECS Fargate backend-backend-1!' });
 });
 
 // Start the server
